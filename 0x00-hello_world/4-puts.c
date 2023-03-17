5430fdb56f3970bcb22f2b52e4071ee611ca3924
Mmaphaka <mmaphakamb@gmail.com>
2023-03-17 07:23:47 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a newline.
+ * main - A program that prints a line with puts function
  * Return: Always 0.
  */
 
