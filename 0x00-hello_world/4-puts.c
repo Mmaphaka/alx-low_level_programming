63cc1da9f88dfcbeb95a97ce5b5274abb8b5f3aa
Mmaphaka <mmaphakamb@gmail.com>
2023-03-17 07:08:20 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - prints "Programming is like building a multilingual puzzle, followed by a newline.
  * Return: Always 0.
@@ -9,7 +8,6 @@
 int main(void)
 
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
